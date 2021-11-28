46d8282c58e07614a0cf93e3e654309c4c9aee51
genie <luvbliny@gmail.com>
2021-11-28 17:44:25 +0900

    feat : jwt token applied

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -3,9 +3,16 @@ import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [
+    TypeOrmModule.forFeature([User]),
+    JwtModule.register({
+      secret: 'jwtConstants.secret',
+      signOptions: { expiresIn: '24h' },
+    }),
+  ],
   controllers: [UsersController],
   providers: [UsersService],
 })
